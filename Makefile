281c63fdd77a2e1a5a959b7a89099023adb87498
Maxime Ancelin <ancelin.maxime@gmail.com>
2020-04-24 13:45:41 +0200

    create programaticaly multiple obstructors

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	python obstructor.py 200 50
+	python obstructor.py 200 50 0 0 50 600 0 0 200 50 0 800 50 600 0 800
--- obstructor.py
+++ obstructor.py
@@ -3,11 +3,12 @@ from tkinter import ttk
 import argparse
 import sys
 from pynput.keyboard import Key, Listener
+import ipdb
 
-w = 400
-h = 200
-app = None
-
+obstructor1 = None
+obstructor2 = None
+obstructor3 = None
+obstructor4 = None
 
 
 def number_or_default(s, default):
@@ -19,33 +20,59 @@ def number_or_default(s, default):
 
 
 def on_keyboard_press(key):
-    global app
+    global obstructor1
     if (key == Key.esc):
-        app.destroy()
-
+        _g = globals()
+        for i in range(1, 5):
+            obstructor_name = "obstructor{}".format(i)
+            _g[obstructor_name].destroy()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("obstructor")
-    parser.add_argument("w", help="width of obstructor")
-    parser.add_argument("h", help="height of obstructor")
+    for i in range(1, 5):
+        parser.add_argument(
+            "w{}".format(i),
+            help="width of obstructor {}".format(i))
+        parser.add_argument(
+            "h{}".format(i),
+            help="height of obstructor {}".format(i))
+        parser.add_argument(
+            "x{}".format(i),
+            help="width of obstructor {}".format(i))
+        parser.add_argument(
+            "y{}".format(i),
+            help="height of obstructor {}".format(i))
     args = parser.parse_args()
-    w = number_or_default(args.w, w)
-    h = number_or_default(args.h, h)
-    # app = Example()
-    # app.mainloop()
-    app = tk.Tk()
-    app.title("obstructor")
-    app.geometry("{}x{}".format(w, h))
-    app.configure()
-
-    def close(event):
-        app.destroy()
+    print("args : ", args)
 
     keyboard_listener = Listener(
         on_press=on_keyboard_press)
     keyboard_listener.start()
 
-    app['bg'] = '#000000'
-    app.overrideredirect(True)
-    app.mainloop()
+    _g = globals()
+    for i in range(1, 5):
+        wi = "w{}".format(i)
+        hi = "h{}".format(i)
+        xi = "x{}".format(i)
+        yi = "y{}".format(i)
+        arg_wi = vars(args)[wi]
+        arg_hi = vars(args)[hi]
+        arg_xi = vars(args)[xi]
+        arg_yi = vars(args)[yi]
+        # ipdb.set_trace(context=13)
+        _g[wi] = number_or_default(arg_wi, 40)
+        _g[hi] = number_or_default(arg_hi, 40)
+        _g[xi] = number_or_default(arg_xi, 0)
+        _g[yi] = number_or_default(arg_yi, 0)
+        obstructor_name = "obstructor{}".format(i)
+        _g[obstructor_name] = tk.Tk()
+        _g[obstructor_name].title(obstructor_name)
+        _g[obstructor_name].geometry("{}x{}+{}+{}".format(_g[wi], _g[hi], _g[xi], _g[yi]))
+        _g[obstructor_name].configure()
+        _g[obstructor_name]['bg']='#000000'
+        _g[obstructor_name].overrideredirect(True)
+    print(_g)
+    for i in range(1, 5):
+        obstructor_name = "obstructor{}".format(i)
+        _g[obstructor_name].mainloop()
