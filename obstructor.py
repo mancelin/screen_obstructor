b8cbe269a0d0410dabf48402a8429c406475e264
Maxime Ancelin <ancelin.maxime@gmail.com>
2020-04-24 13:00:51 +0200

    clean code

--- obstructor.py
+++ obstructor.py
@@ -8,38 +8,6 @@ w = 400
 h = 200
 app = None
 
-"""
-class Example(tk.Tk):
-    def __init__(self):
-        app = tk.Tk.__init__(self)
-        app.root.title("obstructor")
-        # root.__init__(self)
-        # root.title("obstructor")
-        self.floater = FloatingWindow(self)
-
-
-class FloatingWindow(tk.Toplevel):
-    def __init__(self, *args, **kwargs):
-        tk.Toplevel.__init__(self, *args, **kwargs)
-        self.overrideredirect(True)
-        self.wm_geometry("{}x{}".format(w, h))
-
-        self.label = tk.Label(self, text="")
-        self.label.pack(side="top", fill="both", expand=True)
-
-        self.grip = ttk.Sizegrip(self)
-        self.grip.place(relx=1.0, rely=1.0, anchor="se")
-        self.grip.lift(self.label)
-        self.grip.bind("<B1-Motion>", self.OnMotion)
-
-    def OnMotion(self, event):
-        x1 = self.winfo_pointerx()
-        y1 = self.winfo_pointery()
-        x0 = self.winfo_rootx()
-        y0 = self.winfo_rooty()
-        self.geometry("%sx%s" % ((x1-x0), (y1-y0)))
-        return
-"""
 
 
 def number_or_default(s, default):
