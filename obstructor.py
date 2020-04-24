2de151ad39b6833f9a5bdb3b3d72ded08a70f773
Maxime Ancelin <ancelin.maxime@gmail.com>
2020-04-24 11:39:05 +0200

    WIP argparse

--- obstructor.py
+++ obstructor.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+import argparse
 
+
+w = 400
+h = 200
 
 class Example(tk.Tk):
     def __init__(self):
@@ -9,10 +13,14 @@ class Example(tk.Tk):
 
 
 class FloatingWindow(tk.Toplevel):
-    def __init__(self, *args, **kwargs):
-        tk.Toplevel.__init__(self, *args, **kwargs)
+    def __init__(self, extra_args):
+        tk.Toplevel.__init__(self)
+        print(extra_args)
+        # extra_args = args[1]
+        # print(kwargs)
         self.overrideredirect(True)
-        self.wm_geometry("400x400")
+        # self.wm_geometry("400x400")
+        self.wm_geometry("{}x{}".format(w, h))
 
         self.label = tk.Label(self, text="")
         self.label.pack(side="top", fill="both", expand=True)
@@ -30,6 +38,10 @@ class FloatingWindow(tk.Toplevel):
         self.geometry("%sx%s" % ((x1-x0), (y1-y0)))
         return
 
-
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser("obstructor")
+#     parser.add_argument("w", help="width of obstructor")
+#     args = parser.parse_args()
+#     # if args.w =
 app = Example()
 app.mainloop()
